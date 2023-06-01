d036f47eb07b7402fa7898ad7e49e1ccaa2476cd
Ethan_Hadley <ekhadley@gmail.com>
2023-06-01 09:26:35 -0500

    cleaning

--- funcs.py
+++ funcs.py
@@ -71,146 +71,13 @@ def showMatch(pcList, pcs, sides, thickness=1, scale=1):
     im = cv2.polylines(im, np.int32([s2]), False, (50, 0, 250), thickness) # red is s2
     return imscale(im, scale)
 
-def correctPlacement(p, pos):
-    idx = 10*pos[1]+pos[0]
-    return (idx, p.cheat[idx])
 
-def correctRank(p, pos, matches):
-    idx, rot = correctPlacement(p, pos)
-    for i, m in enumerate(matches):
-        if (m[0] == idx) and (m[2] == rot): return i
-    assert 0, "correct placement not found in matches. :/"
-
-def isTruePairing(p, pcs, sides, cheat):
-    pc1, pc2 = pcs
-    r1, r2 = cheat[pc1], cheat[pc2]
-    s1, s2 = sides
-    #print(f"idx: {pc1, pc2}   rots: {r1, r2}")
-    if pc1 == pc2:
-        return False
-    if (pc2-pc1 == 1) and (pc2%10 != 0):
-        return (r1+3)%4==s1 and (r2+1)%4==s2
-    if (pc2-pc1 == -1) and (pc1%10 != 0):
-        return (r1+1)%4==s1 and (r2+3)%4==s2
-    if pc2-pc1 == 10:
-        return (r1+2)%4==s1 and r2==s2
-    if pc2-pc1 == -10:
-        return r1==s1 and (r2+2)%4==s2
-    return False
-
-def metric(p):
-    comp, diff = [], 0
-    cheat = findCheat(p)
-    for p1 in tqdm(range(len(p.pcs)), desc=f"{cyan}evaluating true vs best scores. . .{endc}", ncols=100, unit="it"):
-        for s1 in range(4):
-            best = None
-            true = None
-            for p2 in range(len(p.pcs)):
-                for s2 in range(4):
-                    if p1 != p2:
-                        fit = p.evalMatch((p1, p2), (s1, s2))
-                        if best==None or fit < best[2]:
-                            best = [(p1, p2), (s1, s2), round(fit, 5)]
-                        if isTruePairing(p, (p1, p2), (s1, s2), cheat):
-                            true = [(p1, p2), (s1, s2), round(fit, 5)]
-            if true != None:
-                if true[2] != 0: diff += (true[2]-best[2])/true[2]
-                else: diff += 1e6
-                comp.append([best, true])
-    return comp
-
-def metricInfo(comp, all=False, thresh=5):
-    scores, bestscores, scales, incompatibles = [], [], [], []
-    worstTrueScore, worstScale = comp[0], comp[0]
-    numcorrect = 0
-    for m in comp:
-        sb, st = m[0][2], m[1][2]
-        s = (st-sb)/st
-        scores.append(m[1][2])
-        bestscores.append(m[0][2])
-        scales.append(s)
-        if s == 0: numcorrect += 1
-        if m[1][2] > worstTrueScore[1][2]: worstTrueScore = m
-        if s > (worstScale[1][2]-worstScale[0][2])/worstScale[1][2]: worstScale = m
-        if m[1][2] > thresh: incompatibles.append(m)
-    if all: [print(e) for e in comp]
-    worstScaleVal = (worstScale[1][2]-worstScale[0][2])/worstScale[1][2]
-    print(f"{len(comp)} true matches:")
-    print(f"{blue}avg {green}true{blue} score: {sum(scores)/len(scores):.5f}, avg {red}best{blue} score: {sum(bestscores)/len(bestscores):.5f}")
-    print(f"{green}avg scale diff to best match: {sum(scales)/len(scales):.5f}")
-    print(f"{purple}number of correct best fits: {numcorrect}/{len(comp)}")
-    print(f"{cyan}worst true match (absolute): {worstTrueScore}")
-    print(f"{yellow}worst true match (scale): {worstScale} with scale {worstScaleVal:.5f}")
-    print(f"{red}true matches worse than {thresh}:")
-    [print(e) for e in incompatibles]
-    print(endc)
-
-def findCheat(p):
-    rots = [1]
-    print("i must be dreaming. . .")
-    sta = p.state.copy()
-    for i in range(1, 60):
-        pos = (i%10, math.floor(i/10))
-        x, y = pos
-        best, rot = None, None
-        if (x==0 or x==9 or y==0 or y==5):
-            for r in range(4):
-                if p.validEdgePlacement(i, pos, r):
-                    #print(r)
-                    rot = r
-        else:
-            for r in range(4):
-                s = p.evalPlacement(i, pos, r, state=sta)
-                #print(s)
-                if (best == None) or (s < best[1]):
-                    best = [r, s]
-                    rot = r
-        sta.place(i, pos, rot, True)
-        #p.showState()
-        rots.append(rot)
-    return rots
-
-def placementFeedback(p, i, zz, poss, pos, rots, sc2, t):
-    idx = 10*pos[1]+pos[0]
-    cz, crot = idx, p.cheat[idx]
-    sc1 = p.evalPlacement(cz, pos, crot)
-    delta = round(time.time()-t, 5)
-    if type(zz) == list:
-        confirm = ""
-        for j in range(len(zz)):
-            if zz[j]==10*poss[j][1]+poss[j][0]: confirm += (f"{bcolors.OKGREEN} (CORRECT)")
-            else: confirm += (f"{bcolors.FAIL} (INCORRECT)")
-        print(f"{bcolors.OKCYAN}{i}: prediction: pcs:{zz} at {poss} with rotations {rots} and a score of {sc2:.5f} {confirm}{bcolors.WARNING} [{delta}s]")
-    else:
-        if zz==10*poss[1]+poss[0]: confirm = f"{bcolors.OKGREEN} (CORRECT)"
-        else: confirm = f"{bcolors.FAIL} (INCORRECT)"
-        print(f"{bcolors.OKCYAN}{i}: prediction: pcs:{zz} at {pos} with rotation {rots} and a score of {sc2:.5f} {confirm}{bcolors.WARNING} [{delta}s]")
-
-def listSim(a, b, shift, returnDisp=False):
-    a, b = np.array(a), np.array(b)
-    if len(b) > len(a):
-        a, b = b, a
-    #ran = np.arange(0, len(b), len(b)/len(a)).round().astype(np.int32)
-    ran = np.linspace(0, len(b), len(a), endpoint=False).round().astype(np.int32)
-    b = b[ran]
-    dist = np.sum((a-b)**2, axis=1)**.5
-    avgDist = dist.sum()/len(dist)
-    if shift:
-        disp = np.sum(a-b, axis=0)/len(a)
-        dispMag = math.sqrt(disp[0]**2 + disp[1]**2)
-        if returnDisp: return avgDist - dispMag, disp
-        return avgDist - dispMag
-    return avgDist
 
 def rotate(pos, angle, origin = (0, 0)):
     #print(pos, type(pos))
     rmat = np.matrix([[math.cos(angle),-math.sin(angle)],
                       [math.sin(angle), math.cos(angle)]])
     return np.array(np.dot(np.matrix(pos.astype(np.float64)-origin), rmat.T))+origin
-
-def scaleImgSet(img, lower, upper, steps):
-    inc = (upper-lower)/steps
-    return [imscale(img, lower+inc*i) for i in range(steps+1)]
 
 def filter(arr, dThresh):
     n = np.array(arr, copy=True)
@@ -229,36 +96,6 @@ def filter(arr, dThresh):
             if mod:
                 break
     return n
-
-def choices(arr, n, out=[], head=True):
-    if head:
-        out = []
-    out.append(arr)
-    if len(arr) > n:
-        for i, e in enumerate(arr):
-            choices(np.delete(arr, i, 0), n, out, head=False)
-    return np.unique([e for e in out if len(e) == n], axis=0)
-
-def areaDiff(a, b):
-    a1 = cv2.contourArea(a) if len(a) > 0 else 0
-    a2 = cv2.contourArea(b) if len(b) > 0 else 0
-    return abs(a1-a2)
-
-'''
-def match(target, query, retMap = False):
-    map = cv2.matchTemplate(target, query, cv2.TM_SQDIFF_NORMED)
-    minSim, maxSim, maxSimPos, minSimPos = cv2.minMaxLoc(map)
-    return ((maxSimPos, map[maxSimPos[1]][maxSimPos[0]], query, map) if retMap else (maxSimPos, map[maxSimPos[1]][maxSimPos[0]], query))
-'''
-
-def splitImage(img, dim):
-    sampledim = np.shape(img)
-    subdims = (sampledim[0]//dim[1], sampledim[1]//dim[0])
-    subs = []
-    for j in range(0, dim[0]):
-        for i in range(0, dim[1]):
-            subs.append(img[subdims[0]*j:subdims[0]*(j+1), subdims[1]*i:subdims[1]*(i+1),])
-    return subs
 
 def rectangles(img, posList, dim, weight=5, color=(90, 0, 255)):
     for i, pos in enumerate(posList):
--- search.py
+++ search.py
@@ -6,8 +6,8 @@ imgdir = "C:\\Users\\ek\\Desktop\\testimgs\\yeet\\imgs"
 infodir = "C:\\Users\\ek\\Desktop\\testimgs\\yeet\\extracted"
 pw, ph = 18, 18
 
-#pcs = piece.makePcs("", pw*ph, load=infodir)
-pcs = piece.makePcs(imgdir, pw*ph, load=infodir)
+pcs = piece.makePcs("", pw*ph, load=infodir)
+#pcs = piece.makePcs(imgdir, pw*ph, load=infodir)
 #pcs = piece.makePcs(imgdir, pw*ph, save=infodir)
 #pcs = piece.makePcs(imgdir, 60)
 
@@ -16,7 +16,7 @@ piece.checkPcs(pcs, (pw, ph))
 inps = [(0,(0,0),0), (17,(17,0),0), (306,(0,17),0), (323,(17,17),0)]
 initial = bState.boardState((pw, ph), len(pcs))
 solver = puzl.aStarSolver(initial, pcs, initialPlacement=inps[0])
-best = solver.solve(printEvery=10, heuristicScale=.8, maxRank=10, maxScore=30)
+best = solver.solve(printEvery=3, heuristicScale=.6, maxRank=10, maxScore=30)
 
 
 while 1: cv2.waitKey(1)
